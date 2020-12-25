3ad05d7b4be6b1d26818807c692f062c956f2de4
thangnkHust <thangnk.hust@gmail.com>
2020-12-25 10:00:58 +0700

    Force https for production

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -76,7 +76,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-            ->middleware('api')
+            ->middleware('api', 'cors')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
