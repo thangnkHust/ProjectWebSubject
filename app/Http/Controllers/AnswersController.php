9b42cdb35de272c6bde801d7985a141c3071ecbd
thangnkHust <thangnk.hust@gmail.com>
2020-12-26 11:15:03 +0700

    Edit answer of question

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -6,6 +6,7 @@ use App\Answer;
 use App\Question;
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Gate;
 
 class AnswersController extends Controller
 {
@@ -41,9 +42,12 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        if(Gate::denies('update-answer', $answer)){
+            \abort(403, "Access denied");
+        }
+        return view('answers.edit', \compact('answer'));
     }
 
     /**
@@ -53,9 +57,17 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        if(Gate::denies('update-answer', $answer)){
+            \abort(403, "Access denied");
+        }
+
+        $answer->update($request->validate([
+            'body' => 'required',
+        ]));
+
+        return \redirect()->route('questions.show', $answer->question->slug)->with('success', 'Your answer has been updated');
     }
 
     /**
@@ -66,6 +78,9 @@ class AnswersController extends Controller
      */
     public function destroy(Answer $answer)
     {
-        //
+        if(Gate::denies('delete-answer', $question)){
+            \abort(403, "Acess denied");
+        }
+
     }
 }
